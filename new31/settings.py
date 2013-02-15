ac73363ca5942f83a2d3369d900569817d7370cd
leiddx <leiddx@vip.qq.com>
2013-02-16 00:29:51 +0800

    New APP for tag
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- new31/settings.py
+++ new31/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -117,6 +117,7 @@ TEMPLATE_DIRS = (
 APPS = [
     'shop',
     'cart',
+    'tag',
 ]
 
 DANJO_APPS = [
---
+++ tag/views.py
@@ -0,0 +1,13 @@
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader, Context
+import datetime
+
+def hello(request):
+    now = datetime.datetime.now()
+    t = loader.get_template('tag.htm')
+    html = t.render(Context({'current_date': now}))
+    return HttpResponse(html)
+
+def returnFrist(request):
+    return HttpResponseRedirect("../")
