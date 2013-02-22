e41f40f34bbd13465d65822a43310a3bde8e150d
leiddx <leiddx@vip.qq.com>
2013-02-22 17:35:09 +0800

    nothing
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- shop/models.py
+++ shop/models.py
@@ -7,5 +7,12 @@ class AttriBute(models.Model):
 class Discount(models.Model):
     discount = models.CharField(max_length=30)
 
+class Ad(models.Model):
+    url = models.CharField(max_length=60)
+    img = models.CharField(max_length=60)
+    show = models.BooleanField()
+    vieworder = models.IntegerField()
 
-
+class Config(models.Model):
+    code = models.CharField(max_length=30)
+    value = models.CharField(max_length=30)
