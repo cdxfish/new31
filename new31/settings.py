0c72eb110854c1f5fcaa12b027462bc6708bc4fd
leiddx <leiddx@vip.qq.com>
2013-03-19 01:00:38 +0800

    修正 生产环境与开发环境 调试切换开关
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- new31/settings.py
+++ new31/settings.py
@@ -113,7 +113,7 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.eggs.Loader',
 )
 
-MIDDLEWARE_CLASSES = (
+djangoMidClass = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,8 +121,15 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
+]
+
+if DEBUG:
+    djangoMidClass += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
+
+
+MIDDLEWARE_CLASSES = tuple(djangoMidClass)
+
+
 
 ROOT_URLCONF = 'new31.urls'
 
@@ -162,7 +169,7 @@ APPS = [
     'order',
 ]
 
-DANJO_APPS = [
+djangoAPPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
@@ -173,26 +180,14 @@ DANJO_APPS = [
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
-    'debug_toolbar',
 ] + APPS
 
+if DEBUG:
+    djangoAPPS += ['debug_toolbar',]
+    INTERNAL_IPS = ('127.0.0.1',)
 
-INSTALLED_APPS = tuple(DANJO_APPS)
+INSTALLED_APPS = tuple(djangoAPPS)
 
-
-INTERNAL_IPS = ('127.0.0.1',)
-
-DEBUG_TOOLBAR_PANELS = ( 
-        'debug_toolbar.panels.version.VersionDebugPanel',
-        'debug_toolbar.panels.timer.TimerDebugPanel',
-        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
-        'debug_toolbar.panels.headers.HeaderDebugPanel',
-        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
-        'debug_toolbar.panels.template.TemplateDebugPanel',
-        'debug_toolbar.panels.sql.SQLDebugPanel',
-        'debug_toolbar.panels.signals.SignalDebugPanel',
-        'debug_toolbar.panels.logger.LoggingPanel',
-)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
