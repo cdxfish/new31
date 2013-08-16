6330eda1be2c087fc68c8b107cd547ccd08dc732
leiddx <leiddx@vip.qq.com>
2013-08-16 09:49:23 +0800

    进行图片压缩
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- jpegtran/run.py
+++ jpegtran/run.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 import os
 
-
-
-# os.system('cmd /k')
-for i in os.listdir('old'):
-	os.system('jpegtran.exe -copy none -optimize -perfect old\%s new\%s' % (i, i) )
-	print 'ok ------------ %s' % i
+def jpegtran():
+    for i in os.listdir('old'):
+        os.system('jpegtran.exe -copy none -optimize -perfect old\%s new\%s' % (i, i) )
+        print 'ok ------------ %s' % i
