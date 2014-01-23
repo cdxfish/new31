c1ffc55e2c2fa5d8c8aa6081bba2f0e0dc0a35ae
leiddx <leiddx@vip.qq.com>
2014-01-24 02:38:05 +0800

    修正  - payment 在使用 syncdb 时出现1146错误问题.
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- payment/urls.py
+++ payment/urls.py
@@ -3,13 +3,21 @@ from new31.func import pPatterns
 from views import pays, editPay, subEdit
 from models import Pay
 
+try:
+    urls = []
+    for i in Pay.objects.filter(onl=True):
+        for ii in i.main.urls:
+            urls.append((r'^api\/%s\/%s$' %  (i.cod, ii[0].lstrip('^').rstrip('$')), ii[1], 2))
+
+    urls = tuple(urls)
+
+except Exception, e:
+    # raise e
+    urls = ()
+
 urlpatterns = pPatterns(
     (r'^$', pays, 3),
     (r'^edit\/(?P<id>\d+)\/$', editPay, 3),
     (r'^subedit\/$', subEdit, 3),
-    *(
-        (r'^api\/%s\/%s$' %  (i.cod, ii[0].lstrip('^').rstrip('$')), ii[1], 2)
-        for i in Pay.objects.filter(onl=True)
-            for ii in i.main.urls
-    )
+    *urls
 )
