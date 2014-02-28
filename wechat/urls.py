76e83867e046011774ac17bf14b2c341b7bfdd9a
leiddx <leiddx@vip.qq.com>
2014-02-28 10:58:25 +0800

    修正  - wechat测试
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- wechat/urls.py
+++ wechat/urls.py
@@ -1,10 +1,12 @@
 #coding:utf-8
 from new31.func import pPatterns
 from views import responseMsg, wechatShowTag, checkSignature, queryOrd
+from tag.views import randomShow, tagShow
 
 urlpatterns = pPatterns(
     (r'^$', responseMsg, 2),
     (r'^query\/$', queryOrd, 2),
-    (r'^(?P<tag>.*)\/$', wechatShowTag, 2),
+    (r'^tag\/$', randomShow, 2),
+    (r'^(?P<tag>.*)\/$', tagShow, 2),
     (r'^signature\/$', checkSignature, 2)
 )
