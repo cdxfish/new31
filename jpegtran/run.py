e4a022bfc8bcea90e14bc3fb3860a3d1f1a35595
leiddx <leiddx@vip.qq.com>
2014-03-31 16:08:26 +0800

    修正  - 添加新图片
    Signed-off-by: leiddx <leiddx@vip.qq.com>

--- jpegtran/run.py
+++ jpegtran/run.py
@@ -5,3 +5,7 @@ def jpegtran():
     for i in os.listdir('old'):
         os.system('jpegtran.exe -copy none -optimize -perfect old\%s new\%s' % (i, i) )
         print 'ok ------------ %s' % i
+
+
+if __name__ == '__main__':
+    jpegtran()
